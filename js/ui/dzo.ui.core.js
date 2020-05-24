2b52a7c782af67400f8bd51e0c74f3de25682334
bgrooot <bgrooot@gmail.com>
2020-05-24 09:34:18 +0900

    change data url

--- js/ui/dzo.ui.core.js
+++ js/ui/dzo.ui.core.js
@@ -38,7 +38,7 @@
 
             default:
                 //return 'http://' + domainMapper(bind) + '.chosun.com/site/data/json/' + bind + '.json';
-                return 'https://cdn.jsdelivr.net/gh/bgrooot/dzo-lib@ffa6964/data/' + bind + '.js';
+                return 'https://cdn.jsdelivr.net/gh/bgrooot/dzo-lib@69507ea/data/' + bind + '.js';
         }
     },
 
