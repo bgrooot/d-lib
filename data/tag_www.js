66993282627644c5f72b64a22d21e693bbe2eeb3
bgrooot <bgrooot@gmail.com>
2020-05-24 09:30:49 +0900

    change data

--- data/tag_www.js
+++ data/tag_www.js
@@ -1,95 +1,74 @@
 dzo_json("tag_www",{
     "sample": [
         {
-            "title": "[단독] 10억 기부받아 산 '위안부 쉼터' 펜션처럼 사용하다 돌연 반값 매각",
-            "desc": "[단독] 10억 기부받아 산 '위안부 쉼터' 펜션처럼 사용하다 돌연 반값 매각",
-            "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600091.html",
-            "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600091_0.jpg"
+            "title": "유시민 \"노무현 대통령이 원했던 나라 현실로 다가와\"",
+            "desc": "11주식 추도식 참석해 언급특히 주호영에 \"특별한 환영과 감사\"",
+            "alter": "김아진 기자",
+            "link": "https://news.chosun.com/site/data/html_dir/2020/05/23/2020052300601.html",
+            "image": "https://image.chosun.com/sitedata/image/202005/23/2020052300592_0.jpg"
         },
         {
             "title": "빚 135억·집 팔고 '두리랜드' 숙식 \"세상에 지기 싫습니다\"",
-            "desc": "빚 135억·집 팔고 '두리랜드' 숙식 \"세상에 지기 싫습니다\"",
+            "desc": "[변희원 기자의 한 點] 두리랜드 재개장 배우 임채무",
+            "alter": "변희원 기자",
             "link": "https://news.chosun.com/site/data/html_dir/2020/05/15/2020051503323.html",
             "image": "https://image.chosun.com/sitedata/image/202005/15/2020051503315_0.jpg"
         },
         {
-            "title": "故김복동 할머니 조의금으로 사드반대 단체 지원",
-            "desc": "故김복동 할머니 조의금으로 사드반대 단체 지원",
-            "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600070.html",
-            "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600070_0.jpg"
+            "title": "\"삐뚤빼뚤 그의 유서… 남일 같지 않아 펑펑 울었습니다\"",
+            "desc": "[박돈규기자의 2사 만루] '임계장 이야기' 펴낸 조정진씨",
+            "alter": "박돈규 기자",
+            "link": "https://news.chosun.com/site/data/html_dir/2020/05/22/2020052202687.html",
+            "image": "https://image.chosun.com/sitedata/image/202005/22/2020052202677_0.jpg"
         },
         {
             "title": "[단독] 연세대 교수 300명, 교육부 감사받고 무더기 징계",
-            "desc": "[단독] 연세대 교수 300명, 교육부 감사받고 무더기 징계",
+            "desc": "학과장 69명은 입시 관리 소홀",
+            "alter": '황지윤 기자'
             "link": "http://news.chosun.com/site/data/html_dir/2020/05/16/2020051600072.html",
             "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600072_0.jpg"
         },
         {
             "title": "은퇴·고령자 돈 턴다… '해적뱅킹'하는 은행들",
-            "desc": "은퇴·고령자 돈 턴다… '해적뱅킹'하는 은행들",
+            "desc": "위험상품에 노후자금 투자 유도… 고객돈 수억 펀드에 몰래 넣기도",
+            "alter": "최형석 기자",
             "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600093.html",
             "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600093_0.jpg",
         },
         {
-            "title": "5·18 기념식 불청객된 최강욱…열린민주당 \"행정부 오만하다\"",
-            "desc": "5·18 기념식 불청객된 최강욱…열린민주당 \"행정부 오만하다\"",
-            "link": "https://news.chosun.com/site/data/html_dir/2020/05/15/2020051504258.html",
-            "image": "https://image.chosun.com/sitedata/image/202005/15/2020051504250_0.jpg"
+            "title": "'회장님' 시대는 끝, '소통 전도사'가 된 젊은 총수들",
+            "desc": "직원들과 수시로 만나 소통 '번개 저녁모임'에 셀카 촬영도 OK",
+            "alter": "김강한 기자",
+            "link": "https://news.chosun.com/site/data/html_dir/2020/05/22/2020052202880.html",
+            "image": "https://image.chosun.com/sitedata/image/202005/22/2020052202870_0.jpg"
         },
         {
             "title": "확진자 다녀간 교회… 780명 '기본의 기적'",
-            "desc": "확진자 다녀간 교회… 780명 '기본의 기적'",
+            "desc": "마스크 쓰기, 장갑끼기, 띄어앉기… 방역수칙 지켜, 예배자 전원 음성",
+            "alter": "인천=고석태 기자, 배준용 기자",
             "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600099.html",
             "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600099_0.jpg"
         },
         {
-            "title": "사라진 111층의 꿈…8조 황금 땅에 결국 아파트 건설",
-            "desc": "사라진 111층의 꿈…8조 황금 땅에 결국 아파트 건설",
-            "link": "https://realty.chosun.com/site/data/html_dir/2020/05/14/2020051404016.html",
-            "image": "https://image.chosun.com/sitedata/image/202005/14/2020051403978_0.jpg"
+            "title": "[who] 트럼프에 물렸다… 바이든의 아픈 손가락, 둘째아들",
+            "desc": "기업 로비스트인 차남 헌터",
+            "alter": "정시행 기자",
+            "link": "https://news.chosun.com/site/data/html_dir/2020/05/23/2020052300100.html",
+            "image": "https://image.chosun.com/sitedata/image/202005/23/2020052300100_0.jpg"
         },
         {
-            "title": "국회의원이 도지사에게 넙죽 큰절했다, 환호성이 터져나왔다",
-            "desc": "국회의원이 도지사에게 넙죽 큰절했다, 환호성이 터져나왔다",
-            "link": "https://news.chosun.com/site/data/html_dir/2020/05/15/2020051504373.html",
-            "image": "https://image.chosun.com/sitedata/image/202005/15/2020051504365_0.jpg"
+            "title": "[과학TALK] 이재용 ‘퀀텀점프’ 노리는 비메모리반도체... 5G·AI 타고 성장률 메모리의 5배",
+            "desc": "비메모리=두뇌 역할 반도체 설계 팹리스 + 수탁 생산 파운드리",
+            "alter": "김윤수 기자",
+            "link": "https://biz.chosun.com/site/data/html_dir/2020/05/23/2020052300266.html",
+            "image": "https://image.chosun.com/sitedata/image/202005/23/2020052300257_0.jpg"
         },
         {
-            "title": "\"황교안 미쳐 날뛰어\"…떠나는 김재원의 '반전 매력'",
-            "desc": "\"황교안 미쳐 날뛰어\"…떠나는 김재원의 '반전 매력'",
-            "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600924.html",
-            "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600918_0.jpg"
+            "title": "지적 장애인 30년 食母로 부린 70대 여성 집행유예",
+            "desc": "지적 장애 여성을 30년 넘게 식모(食母)로 부리며 그녀의 장애 수당을 빼앗고, 골프 스윙 연습 봉으로 상습 폭행한 70대 여성에게 법원이 징역형 집행유예를 선고했다.",
+            "alter": "이동휘 기자",
+            "link": "https://news.chosun.com/site/data/html_dir/2020/05/23/2020052300727.html",
+            "image": "https://image.chosun.com/sitedata/image/202005/23/2020052300718_0.jpg"
         },
     ],
-    "article1": {
-        "title": "[단독] 10억 기부받아 산 '위안부 쉼터' 펜션처럼 사용하다 돌연 반값 매각",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600091.html",
-        "alter": "박상현 기자, 이해인 기자",
-        "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600091_0.jpg"
-    },
-    "article2": {
-        "title": "빚 135억·집 팔고 '두리랜드' 숙식 \"세상에 지기 싫습니다\"",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/05/15/2020051503323.html",
-        "alter": "변희원 기자",
-        "image": "https://image.chosun.com/sitedata/image/202005/15/2020051503315_0.jpg"
-    },
-    "article3": {
-        "title": "故김복동 할머니 조의금으로 사드반대 단체 지원",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600070.html",
-        "alter": "이해인 기자",
-        "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600070_0.jpg"
-    },
-    "article4": {
-        "title": "[단독] 연세대 교수 300명, 교육부 감사받고 무더기 징계",
-        "link": "http://news.chosun.com/site/data/html_dir/2020/05/16/2020051600072.html",
-        "alter": "황지윤 기자",
-        "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600072_0.jpg"
-
-    },
-    "article5": {
-        "title": "은퇴·고령자 돈 턴다… '해적뱅킹'하는 은행들",
-        "link": "https://news.chosun.com/site/data/html_dir/2020/05/16/2020051600093.html",
-        "alter": "최형석 기자",
-        "image": "https://image.chosun.com/sitedata/image/202005/16/2020051600093_0.jpg"
-    }
 });
